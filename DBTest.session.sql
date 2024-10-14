e944ca3fa82560799cb21a296c2a8f83a5c76b37
Jahanzaibali2 <Jahanzaibalikhawaja26@gmail.com>
2024-10-14 19:57:20 +0500

    git

--- DBTest.session.sql
+++ DBTest.session.sql
@@ -1,1 +1,1 @@
-CREATE TABLE Employees(Emp_name VARCHAR)
+SELECT * FROM emp
--- filing.py
+++ filing.py
@@ -33,3 +33,65 @@ file.close()
 file = open("text.txt", 'r' )
 print(file.read())
 file.close()
+
+
+# ------------------------------------------------------
+
+
+n = 'Y'
+record = []
+found = False
+
+students = open("data.txt", 'a+')
+while n == 'Y' or n == 'y':
+
+    sname = input("enter student name: " )
+    smarks = int(input("enter marks: "))
+    onerecord = {'Name': sname, 'Marks': smarks}
+    students.seek(0,0)
+    for line in students:
+        line = line.strip()
+        if not line:
+            continue
+        temp = json.loads(line)
+        if temp['Name'] == sname:
+            print("Record Already Exists!")
+            found = True
+            break
+    if found != True:
+        record.append(onerecord)
+        students.write(json.dumps(onerecord))
+        students.write('\n')        
+    n = input("Enter another record?(Y/N)")
+
+students.close()
+
+students = open("data.txt", 'r')
+record = []
+for line in students:
+    line = line.strip()
+    if not line:
+        continue
+    onerecord = json.loads(line.strip())
+    print(f"{onerecord}\n")
+    record.append(onerecord)
+
+# print(record)
+
+# sname = input("Enter Sname: ")
+# smarks = int(input("ENter new marks: "))
+
+# for i in range(len(record)):
+#     if record[i]['Name']==sname:
+#         record[i]['Marks']=smarks
+
+
+# students = open("data.txt","w")
+# students.write(json.dumps(record))
+# students.close()
+
+# students = open("data.txt","r")
+# data = students.read()
+
+# record = json.loads(data)
+# print(record)
